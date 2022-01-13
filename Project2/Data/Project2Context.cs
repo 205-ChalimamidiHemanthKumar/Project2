261f2788cb777368fe6039ebf249138b92e24078
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-13 14:55:50 +0530

    Add project files.

---
+++ Project2/Data/Project2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project2.Controllers;
+
+namespace Project2.Data
+{
+    public class Project2Context : DbContext
+    {
+        public Project2Context (DbContextOptions<Project2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project2.Controllers.Employee> Employee { get; set; }
+    }
+}
